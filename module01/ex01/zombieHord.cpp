af30fa3ab39e7d6467fca2a8325030bb8e32547c
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-05-05 02:20:48 +0700

    fix space EOF

--- module01/ex01/zombieHord.cpp
+++ module01/ex01/zombieHord.cpp
@@ -6,7 +6,7 @@
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 11:51:28 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/05/03 18:44:54 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/05/03 23:17:04 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ Zombie	*zombieHord(int N, std::string name)
 {
 	Zombie *hord;
 	
-	hord = new Zombie[N];
-
+	hord = new (std::nothrow) Zombie[N];
+	
+	if (hord == nullptr)
+	{
+		std::cout << "Memory allocation failed" << std::endl;
+		return (nullptr);
+	}
 	for (int i = 0; i < N ; i++)
 		hord[i].setName(name);
 	return (hord);
