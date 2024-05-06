428618fff241d08e3cc80bcb71194eb271f35143
Polawat Ruenruay <pruenrua@z3t3c4.42bangkok.com>
2024-05-06 21:18:04 +0700

    fix sed

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -15,19 +15,8 @@
 
 int main(int ac, char **av)
 {
-	if (ac != 4)
-		return (std::cerr << "Invalid number of arguments\naccept only <filename> <string1> <string2>" << std::endl, 1);
-		
 	std::fstream in_file;
 	std::fstream out_file;
-	
-	in_file.open(av[1], std::ios::in);
-	if (in_file.rdstate() != 0)
-		return (std::cerr << "Error: " << av[1] << " cannot be open" << std::endl, 1);
-
-	out_file.open(std::string(av[1]) + ".replace", std::ios::out | std::ios::trunc);
-	if (out_file.rdstate() != 0)
-		return (std::cerr << "Error: " << av[1] << ".replace cannot access or somthing went wrong" << std::endl, 1);
 
 	std::string lineRead;
 	std::string leftStr;
@@ -35,33 +24,47 @@ int main(int ac, char **av)
 	std::string strToReplace;
 	std::string replaceStr;
 	std::string resultLine;
-	std::size_t foundIndex;
+	std::string inFileName;
+	std::string outFileName;
+	std::size_t inLineFound;
 
+	if (ac != 4)
+		return (std::cerr << "Invalid number of arguments\naccept only <filename> <string1> <string2>" << std::endl, 1);
+
+	inFileName.assign(av[1]);
+	outFileName.assign(av[1]);
 	strToReplace.assign(av[2]);
 	replaceStr.assign(av[3]);
-	
-	while (in_file.failbit != true)
+
+	if (replaceStr.empty() == true || strToReplace.empty() == true)
+		return (std::cerr << "the <string> should not be empty" << std::endl, 1);
+
+	in_file.open(inFileName.c_str(), std::ios::in);
+	if (in_file.rdstate() != 0)
+		return (std::cerr << "Error: " << inFileName << " cannot be open\ncheck if the file is valid or have correct permission" << std::endl, 1);
+	out_file.open((outFileName + ".replace").c_str(), std::ios::out | std::ios::trunc);
+	if (out_file.rdstate() != 0)
+		return (std::cerr << "Error: " << outFileName << ".replace cannot access or somthing went wrong" << std::endl, 1);
+
+	std::getline(in_file, lineRead, '\0');
+	std::cout << "read : " << lineRead << std::endl;
+	resultLine = lineRead;
+	inLineFound = resultLine.find(strToReplace);
+	while ((strToReplace != replaceStr) && (inLineFound != std::string::npos))
 	{
-		std::cout << "herer " << lineRead << std::endl;
-		std::getline(in_file, lineRead);
-		if (in_file.eof() == true)
+		leftStr = resultLine.substr(0, inLineFound);
+		rightStr = resultLine.substr((inLineFound + strToReplace.length()), resultLine.length());			
+		resultLine = leftStr + replaceStr + rightStr;
+		inLineFound = rightStr.find(strToReplace);
+		if (inLineFound == std::string::npos)
 			break;
-		resultLine = lineRead;
-		foundIndex = resultLine.find(strToReplace);
-		
-		while (strToReplace != replaceStr && foundIndex != std::string::npos)
-		{
-			leftStr = resultLine.substr(0, foundIndex);
-			
-			rightStr = resultLine.substr((foundIndex + strToReplace.length()), resultLine.length());
-			
-			resultLine = leftStr + replaceStr + rightStr;
-			std::cout << "herer " << lineRead << std::endl;
-		}
-		
-		out_file << resultLine << std::endl;
-		lineRead.erase();
+		inLineFound += leftStr.length() + replaceStr.length();
 	}
+	std::cout << "here" << std::endl;
+	out_file << resultLine << std::flush;
+	lineRead.erase();
+	resultLine.erase();
+	std::cout << resultLine << std::endl;
 	in_file.close();
 	out_file.close();
 }
